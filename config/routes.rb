be8f9f5d343a79ba22f9b50957d5dd000a59a9b4
s-takenoshita <s.takenoshita@gmail.com>
2022-04-26 15:17:52 +0900

    bootstrap-font-awesome-sass

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'top' => 'homes#top'
+  root to: "homes#top"
   resources :tournaments, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
