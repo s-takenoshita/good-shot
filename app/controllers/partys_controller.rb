5cd7d4db663b6173527955e94d774ac5cb8bc483
s-takenoshita <s.takenoshita@gmail.com>
2022-05-01 13:26:10 +0900

    party controller routes

--- app/controllers/partys_controller.rb
+++ app/controllers/partys_controller.rb
@@ -1,4 +1,44 @@
 class PartysController < ApplicationController
   def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.new(party_params)
+    if @party.save
+      redirect_to partys_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @party = Party.find(params[:id])
+  end
+
+  def update
+    party = Party.find(params[:id])
+    party.update(party_params)
+    redirect_to party_path(party.id)
+  end
+
+  def index
+    @partys = Party.all
+  end
+
+  def show
+    @party = Party.find(params[:id])
+  end
+
+  def destroy
+    @party = Party.find(params[:id])
+    @party.destroy
+    redirect_to partys_path
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:name, :activity_area,description, party_image_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  resources :partys, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :tournaments, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 end
